ed8efa6980bda58b90046d709428e13bbe978469
AMIT KUMAR MAHTO <135497615+Amit-loveCoding@users.noreply.github.com>
2024-11-26 18:54:32 +0530

    Update Register.js

--- Frontend/mern_auth/src/pages/Register.js
+++ Frontend/mern_auth/src/pages/Register.js
@@ -7,7 +7,7 @@ const Register = () => {
 
     const handleRegister = async (formData) => {
         try {
-            const response = await fetch("http://localhost:5000/api/auth/register", {
+            const response = await fetch("https://mern-auth-platform-3.onrender.com/api/auth/register", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(formData),
